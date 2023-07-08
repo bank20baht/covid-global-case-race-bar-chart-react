972eb719e18ab265ced9f4b858fbe2b22def8d44
Nattapong <banker.z412@gmail.com>
2023-07-09 00:07:37 +0700

    create buttom to incress/decress day to get case in each day

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,72 @@
-import { React, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import ChartRace from "react-chart-race";
 import "./styles.css";
-import axios from 'axios'
+import axios from 'axios';
+
 function App() {
   const [data, setData] = useState([]);
-  const [racebar, setReceBar] = useState([])
+  const [racebar, setRaceBar] = useState([]);
+  const [currentDate, setCurrentDate] = useState(new Date(2023, 2, 9));
 
   const fetchData = async () => {
     try {
-      const res = await axios.get('https://disease.sh/v3/covid-19/historical')
-      console.log(res.data)
-      setData(res.data)
+      const res = await axios.get('https://disease.sh/v3/covid-19/historical');
+      console.log(res.data);
+      setData(res.data);
     } catch (error) {
       console.error(error);
     }
-  }
+  };
+
+  const formattedDate = currentDate.toLocaleString('en-US', { month: 'numeric', day: 'numeric', year: '2-digit' });
 
   useEffect(() => {
-    fetchData()
+    fetchData();
   }, []);
+
+  useEffect(() => {
+    handleChange()
+  }, [currentDate])
 
   function handleChange() {
     const outputJson = data.map((item, index) => {
-      console.log(item.timeline.cases.date)
+      console.log(item.timeline.cases.date);
       return {
         id: index,
         title: item.country,
-        value: item.timeline.cases["2/8/23"],
-        color: "#D1B70A"
-      }
-    })
-    console.log(outputJson)
-    setReceBar(outputJson)
-    console.log(data[0].timeline.cases["2/8/23"])
+        value: item.timeline.cases[formattedDate],
+        color: getRandomColor()
+      };
+    });
+    console.log(outputJson);
+    setRaceBar(outputJson);
+  }
+
+  const handlerPrevDate = () => {
+    const newDate = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      currentDate.getDate() - 1,
+    );
+    setCurrentDate(newDate);
+  };
+
+  const handlerNextDate = () => {
+    const newDate = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      currentDate.getDate() + 1,
+    );
+    setCurrentDate(newDate);
+  };
+
+  function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
   }
 
   return (
@@ -40,17 +74,26 @@ function App() {
       <button className="btn" onClick={handleChange}>
         Click Me!
       </button>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'row'
+      }}>
+        <h2 onClick={() => handlerPrevDate()}>{'<< '}</h2>
+        <h2>{formattedDate}</h2>
+        <h2 onClick={() => handlerNextDate()}>{' >>'}</h2>
+      </div>
+
       <ChartRace
         data={racebar}
-        backgroundColor="#000"
+        backgroundColor="#fff"
         width={600}
         padding={12}
         itemHeight={30}
         gap={20}
-        titleStyle={{ font: "normal 400 10px Arial", color: "#fff" }}
+        titleStyle={{ font: "normal 400 10px Arial", color: "#000" }}
         valueStyle={{
           font: "normal 400 11px Arial",
-          color: "rgba(255,255,255, 1)"
+          color: "#000"
         }}
       />
     </div>
